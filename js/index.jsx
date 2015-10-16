9249428220be6408558ffac627602c23307286cd
mishk0 <proverstka@gmail.com>
2015-10-16 11:32:07 -0400

    add auth

--- js/index.jsx
+++ js/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from 'react-router';
-
-const { Route, NotFoundRoute, DefaultRoute, Link, RouteHandler } = Router;
+import { Router, Route, Link } from 'react-router';
 
 class Moment extends React.Component {
     render() {
@@ -12,6 +10,24 @@ class Moment extends React.Component {
                 <div className='moment-pic'>{ this.props.pic }</div>
                 <span className='moment-author'>{ this.props.author }</span>
             </div>
+        )
+    }
+}
+
+class Moments extends React.Component {
+    componentDidMount() {
+        this.getMoments();
+    }
+
+    getMoments() {
+        $.get('https://storia.me/api/feed/content', function() {
+            debugger;
+        })
+    }
+
+    render() {
+        return (
+            <Auth />
         )
     }
 }
@@ -29,15 +45,47 @@ class App extends React.Component {
     }
 }
 
-var routes = (
-    <Route name="app" path="/" handler={App}>
-        <DefaultRoute handler={App}/>
-        <Route name="items" handler={App} />
-        <Route name="item" path="/item/:item" handler={Item} />
-    </Route>
-);
+class Auth extends React.Component {
+    render() {
+        return (
+            <form method="POST" action="https://storia.me/api/acl/auth/Selfish/test_task@example.com">
+                <input type="hidden" name="password" value="qwe123" />
+                <input type="hidden" name="remember" value="false" />
+                <input type="hidden" name="token" value="" />
+                <button>Authorize</button>
+            </form>
+        )
+    }
+}
 
-Router.run(routes, Router.HistoryLocation, function (Handler) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+class NoMatch extends React.Component {
+    render() {
+        return 404;
+    }
+}
+
+function init() {
+    ReactDOM.render((
+        <Router>
+            <Route path="/" component={Moments}>
+                <Route name="items" component={Moments} />
+                <Route path="*" component={NoMatch}/>
+            </Route>
+        </Router>
+    ), document.getElementById('app'));
+}
+
+$.ajax({
+    type: 'POST',
+    url: 'https://storia.me/api/acl/auth/Selfish/test_task@example.com',
+    data: JSON.stringify({password: 'qwe123', remember: false, token: ''}),
+    xhrFields: {
+        withCredentials: true
+    },
+    success: function() {
+        init();
+    },
+    contentType: 'application/json',
+    dataType: 'json'
 });
 
