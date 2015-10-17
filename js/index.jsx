828316f1bedb981010da264ca5a4dda2ce7d5f7c
mishk0 <proverstka@gmail.com>
2015-10-17 04:48:48 -0400

    add moment component

--- js/index.jsx
+++ js/index.jsx
@@ -3,31 +3,95 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router';
 
 class Moment extends React.Component {
+    getPictureUrl() {
+        var attachments = this.props.attachments;
+        var len = attachments.length;
+        var REG_EXP = /\d/g;
+
+        for (var i = len; i--;) {
+            return REG_EXP.test(attachments[i].file.title);
+        }
+
+        return attachments[len - 1];
+    }
+
     render() {
+        var picNode;
+        var picUrl = this.getPictureUrl();
+
+        if (picUrl) {
+            picNode = <div className="moment-pic"><img src="{ picUrl }" /></div>;
+        }
+
         return (
-            <div className='moment'>
-                <div className='moment-title'>{ this.props.title }</div>
-                <div className='moment-pic'>{ this.props.pic }</div>
-                <span className='moment-author'>{ this.props.author }</span>
-            </div>
+            <a href="#" className="moment list-group-item">
+                <h4 className="moment-title">{ this.props.title }</h4>
+
+                <h5 className="moment-story-title">
+                    { this.props.storyTitle }
+                    <span className="moment-author label label-info">{ this.props.owner.name }</span>
+                </h5>
+                { picNode }
+                <button className="btn btn-primary btn-xs" type="button">
+                    Like <span className="badge">{ this.props.stats.likes }</span>
+                </button>
+            </a>
         )
     }
 }
 
 class Moments extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {items: []};
+    }
+
     componentDidMount() {
-        this.getMoments();
+        this.setAuth().then(this.getMoments());
+    }
+
+    setAuth() {
+        return new Promise((resolve) => {
+            $.ajax({
+                type: 'POST',
+                url: 'https://storia.me/api/acl/auth/Selfish/test_task@example.com',
+                data: JSON.stringify({password: 'qwe123', remember: false, token: ''}),
+                xhrFields: {
+                    withCredentials: true
+                },
+                success: function() {
+                    resolve();
+                },
+                contentType: 'application/json',
+                dataType: 'json'
+            });
+        });
     }
 
     getMoments() {
-        $.get('https://storia.me/api/feed/content', function() {
-            debugger;
-        })
+        var that = this;
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://storia.me/api/feed/content',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(data) {
+                that.setState(data);
+            }
+        });
     }
 
     render() {
+        var List = this.state.items.map((item) => {
+            return <Moment key={ item.objectPreview.id } { ...item.objectPreview } />;
+        });
+
         return (
-            <Auth />
+            <div className="list-group">
+                { List }
+            </div>
         )
     }
 }
@@ -36,24 +100,9 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <div className='filters'>
-                    Моменты с картинками | Моменты без картинок
-                </div>
-                <div className='like'>like</div>
+                <h2>Storia</h2>
+                <Moments />
             </div>
-        )
-    }
-}
-
-class Auth extends React.Component {
-    render() {
-        return (
-            <form method="POST" action="https://storia.me/api/acl/auth/Selfish/test_task@example.com">
-                <input type="hidden" name="password" value="qwe123" />
-                <input type="hidden" name="remember" value="false" />
-                <input type="hidden" name="token" value="" />
-                <button>Authorize</button>
-            </form>
         )
     }
 }
@@ -64,28 +113,11 @@ class NoMatch extends React.Component {
     }
 }
 
-function init() {
-    ReactDOM.render((
-        <Router>
-            <Route path="/" component={Moments}>
-                <Route name="items" component={Moments} />
-                <Route path="*" component={NoMatch}/>
-            </Route>
-        </Router>
-    ), document.getElementById('app'));
-}
-
-$.ajax({
-    type: 'POST',
-    url: 'https://storia.me/api/acl/auth/Selfish/test_task@example.com',
-    data: JSON.stringify({password: 'qwe123', remember: false, token: ''}),
-    xhrFields: {
-        withCredentials: true
-    },
-    success: function() {
-        init();
-    },
-    contentType: 'application/json',
-    dataType: 'json'
-});
-
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={App}>
+            <Route name="items" component={App} />
+            <Route path="*" component={NoMatch}/>
+        </Route>
+    </Router>
+), document.getElementById('app'));
